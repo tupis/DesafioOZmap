870eb453fb556f67b450b9d317be3c0ff304098c
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-02-07 02:00:17 -0300

    feat: controller auth login and register

---
+++ src/http/controllers/AuthController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getUserService } from "@modules/user/UserServiceFactory";
+import { UserService } from "@modules/user/services/UserService";
+import { LoginUserDto } from "@modules/user/dto/login-user.dto";
+import { RegisterUserDto } from "@modules/user/dto/register-user.dto";
+
+export class AuthController {
+  constructor(private readonly userService: UserService = getUserService()) {}
+  async login(request: Request, response: Response): Promise<Response> {
+    const { data, status } = await this.userService.authenticateUser(
+      request.body as LoginUserDto,
+    );
+    return response.status(status).json(data);
+  }
+
+  async register(request: Request, response: Response): Promise<Response> {
+    const { data, status } = await this.userService.createUser(
+      request.body as RegisterUserDto,
+    );
+    return response.status(status).json(data);
+  }
+}
