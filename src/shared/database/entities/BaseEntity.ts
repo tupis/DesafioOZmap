682287876b169247f37a60232552f87368a6022d
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-02-07 01:52:18 -0300

    feat: base enity to setup mainly props

---
+++ src/shared/database/entities/BaseEntity.ts
@@ -0,0 +1,16 @@
+import { prop } from "@typegoose/typegoose";
+import { SchemaTypes } from "mongoose";
+
+export class BaseEntity {
+  @prop({ type: SchemaTypes.ObjectId, auto: true })
+  public _id!: string;
+
+  @prop({ type: SchemaTypes.Date, default: () => new Date() })
+  public createdAt!: Date;
+
+  @prop({ type: SchemaTypes.Date, default: () => new Date() })
+  public updatedAt!: Date;
+
+  @prop({ type: SchemaTypes.Date, default: null })
+  public deletedAt!: Date;
+}
