b0157e131996d25932b0c2b4326829186b6ad09f
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-02-11 09:45:12 -0300

    feat: add router new system to map router

---
+++ src/http/routes/router.ts
@@ -0,0 +1,45 @@
+import { validationMiddlewareDto } from "@shared/middleware/validation";
+import { HttpServer } from "./httpRouter";
+import AuthController from "@http/controllers/AuthController";
+import { LoginUserDto } from "@modules/user/dto/login-user.dto";
+import { RegisterUserDto } from "@modules/user/dto/register-user.dto";
+import UserController from "@http/controllers/UserController";
+
+export class Router {
+  private server: HttpServer;
+
+  constructor(server: HttpServer) {
+    this.server = server;
+  }
+
+  init() {
+    this.server.on({
+      path: "",
+      method: "get",
+      handler: async () => {
+        return { body: "Funcionando", statusCode: 200 };
+      },
+      public: true,
+    });
+
+    this.server.on({
+      path: "/auth/login",
+      method: "post",
+      handler: async (req, res) => {
+        return await AuthController.login(req, res);
+      },
+      middleware: [validationMiddlewareDto(LoginUserDto)],
+      public: true,
+    });
+
+    this.server.on({
+      path: "/auth/register",
+      method: "post",
+      handler: async (req, res) => {
+        return await AuthController.register(req, res);
+      },
+      middleware: [validationMiddlewareDto(RegisterUserDto)],
+      public: true,
+    });
+  }
+}
