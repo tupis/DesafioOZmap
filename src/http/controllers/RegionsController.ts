6e8cc38fc7a8a60278e6bba0fba8df3368c7fe26
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-02-12 12:30:08 -0300

    feat: search region by point and near region

--- src/http/controllers/RegionsController.ts
+++ src/http/controllers/RegionsController.ts
@@ -1,4 +1,6 @@
 import { CreateRegionDto } from "@modules/region/dto/create-region.dto";
+import { FindRegionsContainingPointDto } from "@modules/region/dto/find-region-containing-point.dto";
+import { FindRegionsNearPointDto } from "@modules/region/dto/find-regions-near-point.dto";
 import { UpdateRegionDto } from "@modules/region/dto/update-region.dto";
 import { getRegionService } from "@modules/region/RegionServiceFactory";
 import { RegionService } from "@modules/region/services/RegionService";
@@ -49,6 +51,38 @@ class RegionController {
     }
 
     const { data, status } = await this.regionService.findById(id);
+
+    return response.status(status).json(data);
+  }
+
+  async findRegionsContainingPoint(request: Request, response: Response) {
+    const region = request.body as FindRegionsContainingPointDto;
+
+    if (!region) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        data: "Region is required",
+      });
+    }
+
+    const { data, status } =
+      await this.regionService.findRegionsContainingPoint(region);
+
+    return response.status(status).json(data);
+  }
+
+  async findRegionsNearPointDto(request: Request, response: Response) {
+    const region = request.body as FindRegionsNearPointDto;
+
+    if (!request.user) {
+      return response.status(HttpStatus.UNAUTHORIZED).json({
+        data: "User is not authenticated",
+      });
+    }
+
+    const { data, status } = await this.regionService.findRegionsNearPoint(
+      region,
+      request.user.id,
+    );
 
     return response.status(status).json(data);
   }
