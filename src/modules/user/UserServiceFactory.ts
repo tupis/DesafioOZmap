a142f9dfce68c27fc224e51543cc8a4bd8b6e3bf
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-02-14 13:53:10 -0300

    feat: remove injection repository

--- src/modules/user/UserServiceFactory.ts
+++ src/modules/user/UserServiceFactory.ts
@@ -1,12 +1,11 @@
 import { GeoLocationService } from "@shared/services/GeoLocationService";
-import { AddressRepository } from "./repositories/AddressRepository";
+// import { AddressRepository } from "./repositories/AddressRepository";
 import { UserRepository } from "./repositories/UserRepository";
 import { UserService } from "./services/UserService";
 
 export function getUserService(): UserService {
   const userRepository = new UserRepository();
-  const addressRepository = new AddressRepository();
-  const geoLocationService = new GeoLocationService(addressRepository);
+  const geoLocationService = new GeoLocationService();
 
   return new UserService(userRepository, geoLocationService);
 }
